e9e97a1f87d0c24181af3af6ce16f4c79a74a8b6
PedroVidalDev <pedrohvidals@gmail.com>
2024-03-26 17:03:43 -0300

    [feat] - criando rotas para o programa e organizando com arquivo index

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const app = require('./src/app.js');
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log('servidor escutando!');
-});
+app.listen(3000, () => {
+  console.log("online.");
+})
