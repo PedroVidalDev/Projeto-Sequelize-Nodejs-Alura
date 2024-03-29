80a88aa664959bbbf2680470ae8a7e8a16b111b5
PedroVidalDev <pedrohvidals@gmail.com>
2024-03-29 09:18:41 -0300

    [feat] - criando o restante dos metodos de crud

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -13,6 +13,63 @@ class Controller{
             // error
         }
     }
+
+    async pegaUmPorId(req, res){
+        const {id} = req.params;
+        try{
+            const registro = await this.service.pegaUmRegistroPorId(Number(id));
+            return res.status(200).json(registro);
+        }
+
+        catch(e){
+            // erro
+        }
+    }
+
+    async criaNovo(req, res){
+        const dados = req.body;
+
+        try{
+            const novoRegistro = await this.service.criaRegistro(dados);
+            return res.status(200).json(novoRegistro);
+        }
+
+        catch{
+            // erro
+        }
+    }
+
+    async exclui(req, res){
+        const { id } = req.params;
+        try{
+            await this.service.excluiRegistro(Number(id));
+            return res.status(200).json({message: `ID deletado: ${id}`});
+        }
+
+        catch(e){
+            // erro
+        }
+    }
+
+    async atualiza(req, res){
+        const {id} = req.params;
+        const dadosAtualizados = req.body;
+
+        try{
+            const foiAtualizado = await this.service.atualizaRegistro(dadosAtualizados, Number(id));
+            const registro = await this.service.pegaUmRegistroPorId(Number(id));
+            if(foiAtualizado){
+                return res.status(200).json({message: `Registro foi atualizado com sucesso.`, registro})
+            }
+            else{
+                return res.status(400).json({message: `Registro com id ${id} nao foi atualizado.`})
+            }
+        }
+        
+        catch(e){
+
+        }
+    }
 }
 
 module.exports = Controller;
--- src/services/Service.js
+++ src/services/Service.js
@@ -8,6 +8,32 @@ class Service{
     async pegaTodosOsRegistros() {
         return dataSource[this.model].findAll();
     }
+
+    async pegaUmRegistroPorId(id){
+        return dataSource[this.model].findByPk(id);
+    }
+
+    async criaRegistro(dadosDoRegistro){
+        return dataSource[this.model].create(dadosDoRegistro);
+    }
+
+    async excluiRegistro(id){
+        return dataSource[this.model].destroy({where: {id: id}});
+    }
+
+    async atualizaRegistro(dadosAtualizados, id){
+        const listaDeRegistroAtualizado = dataSource[this.model].update(dadosAtualizados, {
+            where: {
+                id: id
+            }
+        });
+
+        if(listaDeRegistroAtualizado[0] == 0){
+            return false;
+        } else{
+            return true;
+        }
+    }
 }
 
 module.exports = Service;
