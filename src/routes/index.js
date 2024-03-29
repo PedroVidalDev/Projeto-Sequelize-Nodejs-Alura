671516b2f0af061a3f5c5320a217ac868223c8fa
PedroVidalDev <pedrohvidals@gmail.com>
2024-03-29 09:57:46 -0300

    [feat] - criando as rotas para cursos e categorias

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const pessoas = require('./PessoaRoutes.js');
+const categorias = require('./CategoriaRoutes.js');
+const cursos = require('./CursosRoutes.js');
 
 module.exports = app => {
     app.use(
         express.json(),
-        pessoas
+        pessoas,
+        categorias,
+        cursos
     )
 }
