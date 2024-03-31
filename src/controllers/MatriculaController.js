0b09a45fc848920375ae1f10f79732b54c00ad6b
PedroVidalDev <pedrohvidals@gmail.com>
2024-03-31 12:59:59 -0300

    [feat] - adicionando rota de contagem de matriculas por estudante

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -10,6 +10,22 @@ class MatriculaController extends Controller{
     constructor(){
         super(matriculaService);
     }
+
+    async pegaMatriculasPorEstudante(req, res){
+        const {estudante_id} = req.params;
+        try{
+            const listaMatriculasPorEstudante = await matriculaService.pegaEContaRegistros({
+                estudante_id: Number(estudante_id),
+                status: 'matriculado'
+            });
+
+            return res.status(200).json(listaMatriculasPorEstudante);
+        }
+
+        catch(erro){
+            return res.status(500).json({erro: erro.message});
+        }
+    }
 }
 
 module.exports = MatriculaController;
